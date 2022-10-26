e7d56152cbc928caaa26fce79e184b6b36f7c739
Dorian Bourcet <dorian.bourcet@libeo.com>
2022-10-26 17:47:39 -0400

    Version 1.0.1
    Signed-off-by: Dorian Bourcet <dorian.bourcet@libeo.com>

--- wikibiographie.php
+++ wikibiographie.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WikiBiographie
  * Plugin URI:        https://www.productionsrhizome.org
  * Description:       Gestion de biographies et récupération d'informations depuis Wikipédia en français.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Productions Rhizome
  * Author URI:        https://www.productionsrhizome.org
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (! defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WIKIBIOGRAPHIE_VERSION', '1.0.0');
+define('WIKIBIOGRAPHIE_VERSION', '1.0.1');
 
 /**
  * The code that runs during plugin activation.
